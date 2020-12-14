8ac8709f5e2eb176accd23d3bed4f6802f0908c5
surajmt8848 <suraj8848mt@gmail.com>
2020-12-14 16:08:17 +0545

    removed unused files

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -4,7 +4,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework import exceptions, status
 from rest_framework.response import Response
-from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from .serializers import RegisterSerializer
 from .helper import generate_access_token, JWTauthentication
 
